c119c890736d4f04007be875e4584d4b3cad2614
nur-rohmad <nurrohmad776@gmail.com>
2022-05-27 20:59:51 +0700

    pengumpulan tugas

--- index.js
+++ index.js
@@ -23,20 +23,36 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    hasilLooping.push(
+      dataYangAkanDilooping[index].namaDepan +
+        " " +
+        dataYangAkanDilooping[index].namaBelakang
+    );
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    if (dataYangAkanDilooping[index].jenisKelamin == "M") {
+      jumlahPria += 1;
+    }
+  }
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
   let jumlahWanita = null;
-
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    if (dataYangAkanDilooping[index].jenisKelamin == "F") {
+      jumlahWanita += 1;
+    }
+  }
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
       yang akan mengomentari apakah lebih banyak Pria atau Wanita
@@ -49,6 +65,13 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
